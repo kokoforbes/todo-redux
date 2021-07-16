9b923865fb22a59fbfb68a84918e4f0a43b28417
onikoko abdullahi <Onikokoabdullahi@gmail.com>
2021-07-16 11:30:56 +0100

    Adedd redux-thunk

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,5 +1,6 @@
-// import { ThunkAction } from "redux-thunk";
-import { Todo } from "./types"
+import { ThunkAction } from "redux-thunk";
+import { Todo, Store } from "./types";
+import { Action } from "redux";
 
 export const ADD_TODO = "ADD_TODO";
 export const DELETE_TODO = "DELETE_TODO";
@@ -8,27 +9,25 @@ export const TOGGLE_TODO = "TOGGLE_TODO";
 export const SET_NEWTODO = "SET_NEWTODO";
 export const SET_TODOS = "SET_TODOS";
 
-
-export type ActionTypes = 
-| {type: typeof SET_TODOS; payload: Todo[] }
-| {type: typeof ADD_TODO }
-| {type: typeof DELETE_TODO; payload: number }
-| {type: typeof UPDATE_TODO; payload: {
-    id: number,
-    text: string
-  }
-}
-| {type: typeof TOGGLE_TODO; payload: number }
-| {type: typeof SET_NEWTODO; payload: string }
-
-
+export type ActionTypes =
+  | { type: typeof SET_TODOS; payload: Todo[] }
+  | { type: typeof ADD_TODO }
+  | { type: typeof DELETE_TODO; payload: number }
+  | {
+      type: typeof UPDATE_TODO;
+      payload: {
+        id: number;
+        text: string;
+      };
+    }
+  | { type: typeof TOGGLE_TODO; payload: number }
+  | { type: typeof SET_NEWTODO; payload: string };
 
 export const addTodo = (): ActionTypes => ({ type: ADD_TODO });
 
-
 export const deleteTodo = (id: number): ActionTypes => ({
   type: DELETE_TODO,
-  payload: id
+  payload: id,
 });
 
 export const updateTodo = (id: number, text: string): ActionTypes => ({
@@ -54,10 +53,10 @@ export const setTodos = (todos: Todo[]): ActionTypes => ({
   payload: todos,
 });
 
-export const getTodos = (
-  url: string
-): ThunkAction<void, Store, unknown, Action<string>> => async (dispatch) => {
-  const resp = await fetch(url);
-  const todos: Todo[] = await resp.json();
-  dispatch(setTodos(todos));
-};
+export const getTodos =
+  (url: string): ThunkAction<void, Store, unknown, Action<string>> =>
+  async (dispatch) => {
+    const resp = await fetch(url);
+    const todos: Todo[] = await resp.json();
+    dispatch(setTodos(todos));
+  };
